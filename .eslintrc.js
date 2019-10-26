72f1d4c694110c96c4d60bd9b64c6422db9b9606
Prashant Palikhe <prashpal09@gmail.com>
2019-10-26 18:19:25 +0200

    chore: setup eslint

---
+++ .eslintrc.js
@@ -0,0 +1,12 @@
+module.exports = {
+  env: {
+    browser: true,
+    node: true,
+  },
+
+  parserOptions: {
+    parser: 'babel-eslint',
+  },
+
+  extends: ['standard', 'plugin:prettier/recommended'],
+};
